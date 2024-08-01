a3f19fc4471fdf18cbd8646600babe41d19dc9c2
Nevin Ozkan <ozkannevin73@gmail.com>
2024-08-01 15:22:57 +0300

    first commit

--- MovieApp/Modules/Main/Controller/MainVC.swift
+++ MovieApp/Modules/Main/Controller/MainVC.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class MainVC: UIViewController {
     
+    private var movies: [Movie] = []
+    private let serviceManager = ServiceManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        fetchUpcomingMovies()
     }
-
-
+    
+    private func fetchUpcomingMovies() {
+        let serviceManager = ServiceManager()
+        serviceManager.fetchUpcomingMovies { [weak self] result in
+            switch result {
+            case .success(let data):
+                do {
+                    let decoder = JSONDecoder()
+                    let response = try decoder.decode(MovieResponse.self, from: data)
+                    self?.movies = response.results
+                    DispatchQueue.main.async {
+                        
+                    }
+                } catch {
+                    print("JSON decode error: \(error)")
+                }
+            case .failure(let error):
+                print("Network error: \(error)")
+            }
+        }
+    }
 }
--- MovieApp/Modules/Main/ViewModel/MainVM.swift
+++ MovieApp/Modules/Main/ViewModel/MainVM.swift
@@ -7,6 +7,63 @@
 
 import Foundation
 
-final class MainVM {
-    
+struct Movie: Decodable {
+    let adult: Bool
+    let backdropPath: String?
+    let genreIds: [Int]
+    let id: Int
+    let originalLanguage: String
+    let originalTitle: String
+    let overview: String
+    let popularity: Double
+    let posterPath: String?
+    let releaseDate: String
+    let title: String
+    let video: Bool
+    let voteAverage: Double
+    let voteCount: Int
+
+   
+    private enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIds = "genre_ids"
+        case id
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview
+        case popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title
+        case video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
+
+
+struct MovieResponse: Decodable {
+    let dates: Dates
+    let page: Int
+    let results: [Movie]
+
+   
+    private enum CodingKeys: String, CodingKey {
+        case dates
+        case page
+        case results
+    }
+}
+
+
+struct Dates: Decodable {
+    let maximum: String
+    let minimum: String
+
+   
+    private enum CodingKeys: String, CodingKey {
+        case maximum
+        case minimum
+    }
 }
