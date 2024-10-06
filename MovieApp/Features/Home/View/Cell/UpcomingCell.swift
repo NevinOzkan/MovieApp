369708c68061aae23030f3b68253846af7fbe9fd
Nevin Ozkan <ozkannevin73@gmail.com>
2024-10-06 15:01:11 +0300

    UpcomingCell

---
+++ MovieApp/Features/Home/View/Cell/UpcomingCell.swift
@@ -0,0 +1,23 @@
+//
+//  UpcomingCell.swift
+//  MovieApp
+//
+//  Created by Nevin Özkan on 5.10.2024.
+//
+
+import UIKit
+
+class UpcomingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
