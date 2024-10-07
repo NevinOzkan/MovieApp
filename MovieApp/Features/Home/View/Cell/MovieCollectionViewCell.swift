9097535baec849e4808201738915e9c5e8fa55e9
Nevin Ozkan <ozkannevin73@gmail.com>
2024-10-07 10:47:09 +0300

    sliderCollection

---
+++ MovieApp/Features/Home/View/Cell/MovieCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCollectionViewCell.swift
+//  MovieApp
+//
+//  Created by Nevin Özkan on 6.10.2024.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
